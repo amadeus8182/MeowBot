7a3552a22061535ee3b3d40b914efa09061541c2
Amadeus8182 <amadeus8182@gmail.com>
2023-01-22 20:28:45 -0600

    Added command meow. Allows users to use 'm.meow' and the bot sends a random cat gif using Tenor API

---
+++ commands/meow.js
@@ -0,0 +1,20 @@
+// require('dotenv').config();
+
+const { EmbedBuilder } = require("@discordjs/builders");
+const { DiscordAPIError } = require("discord.js");
+
+module.exports = {
+    name: 'meow',
+    async execute(msg) {    
+        url = `https://g.tenor.com/v2/search?q=kitty meowing&key=${process.env.TENORKEY}&limit=50`;
+        response = await fetch(url);
+        let json = await response.json();
+
+        const index = Math.floor((Math.random() * json.results.length))
+        let embed = new EmbedBuilder()
+            .setTitle(`meow #${index+1}`)
+            .setImage(json.results[index].media_formats.gif.url)
+            // .setImage(json.results[index].itemurl)
+        msg.channel.send({embeds: [embed]})
+    }
+}
