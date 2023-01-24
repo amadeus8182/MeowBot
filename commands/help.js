3815f6ef16a99e44e08ddc176494879859b7dcf6
Amadeus8182 <amadeus8182@gmail.com>
2023-01-23 21:25:20 -0600

    fix newline display issue for mobile

--- commands/help.js
+++ commands/help.js
@@ -3,11 +3,12 @@ const { EmbedBuilder } = require("@discordjs/builders")
 module.exports = {
     name: 'help',
     execute(msg) {
+        let cmds = ['help - shows you this!',
+                    'meow - sends a cat gif!',
+                    'invite - send an invite link for this bot!']
         let embed = new EmbedBuilder()
             .setTitle('Command List!')
-            .setDescription(`help - shows you this!
-                             meow - sends a cat gif!
-                             invite - send an invite link for this bot!`);
+            .setDescription(cmds.join('\n'));
         msg.channel.send({embeds: [embed]})
     }
 }
