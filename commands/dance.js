8fc94e332c03d382d911087d09e9bfd4d19d2e44
Amadeus8182 <amadeus8182@gmail.com>
2023-01-24 14:22:58 -0600

    Add new command dance. Sends a dancing cat gif

---
+++ commands/dance.js
@@ -0,0 +1,17 @@
+const { EmbedBuilder } = require("@discordjs/builders")
+
+module.exports = {
+    name: 'meow',
+    async execute(msg) {    
+        url = `https://g.tenor.com/v2/search?q=kitty dance&key=${process.env.TENORKEY}&limit=50`;
+        response = await fetch(url);
+        let json = await response.json();
+
+        const index = Math.floor((Math.random() * json.results.length))
+        let embed = new EmbedBuilder()
+            .setTitle(`meow #${index+1}`)
+            .setImage(json.results[index].media_formats.gif.url)
+
+        msg.channel.send({embeds: [embed]})
+    }
+}
