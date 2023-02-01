29aed39cab2b9478032ee8e4e71b32373bc78f67
Amadeus8182 <amadeus8182@gmail.com>
2023-01-31 18:27:54 -0600

    branch: change help command to use the commands list and get info from each command module instead of hard coding each command and its description

--- commands/embed.js
+++ commands/embed.js
@@ -3,9 +3,20 @@ const { EmbedBuilder } = require("@discordjs/builders")
 module.exports = {
 	name: 'embed',
 	execute(msg, args) {
+		let sent = args.join(' ');
+		if(sent.length > 256) {
+			msg.channel.send('Please use a shorter message to embed!');
+			return
+		} else if (sent.length < 1) {
+			msg.channel.send('Please provide a message to embed!');
+			return
+		}
 		let embedding = new EmbedBuilder()
 			.setTitle(args.join(' '))
 			.setFooter({text: msg.author.username, iconURL: msg.author.displayAvatarURL({dynamic: true})}); 
+
+		if(msg.guild!==null) msg.delete();
+
 		msg.channel.send({embeds: [embedding]})
 	}
 }
--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,15 @@
 const { EmbedBuilder } = require("@discordjs/builders")
+const bot = require('../bot.js')
 
 module.exports = {
     name: 'help',
     execute(msg) {
-        let cmds = ['help - shows you this!',
-                    'meow - sends a cat gif!',
-                    'invite - send an invite link for this bot!']
+		let cmds = []
+
+		for(const cmd of bot.cmds) {
+			cmds.push(`**${cmd[1].name}** - ${cmd[1].desc}`);
+		}
+
         let embed = new EmbedBuilder()
             .setTitle('Command List!')
             .setDescription(cmds.join('\n'));
