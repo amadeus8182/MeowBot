78bbbc2f1fc00e9df8701c1bc5da51b2c804b01c
Amadeus8182 <amadeus8182@gmail.com>
2023-01-31 18:50:33 -0600

    limit users to only provide message less than 256 characters

--- commands/embed.js
+++ commands/embed.js
@@ -3,6 +3,14 @@ const { EmbedBuilder } = require("@discordjs/builders")
 module.exports = {
 	name: 'embed',
 	execute(msg, args) {
+		let send = args.join(' ');
+		if(send.length > 256) {
+			msg.channel.send('Please provide a shorter message to embed!');
+			return
+		} else if(send.length < 1) {
+			msg.channel.send('Please provide a message to embed!');
+			return
+		}
 		let embedding = new EmbedBuilder()
 			.setTitle(args.join(' '))
 			.setFooter({text: msg.author.username, iconURL: msg.author.displayAvatarURL({dynamic: true})}); 
