dc108744a808e87a8c1026902c0f13def04dd714
Amadeus8182 <amadeus8182@gmail.com>
2023-01-31 19:20:10 -0600

    fix bug on invite.js. forgot a comma at the end of the description

--- commands/invite.js
+++ commands/invite.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'invite',
-	desc: 'sends an invite link for the bot to be able to join other servers.'
+	desc: 'sends an invite link for the bot to be able to join other servers.',
     execute(msg) {
         msg.channel.send('You can invite using this link! https://discord.com/oauth2/authorize?client_id=1066857245675102360&scope=bot&permissions=274878032960');
     }
