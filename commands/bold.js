2f7b3f9e080f776d85667adc48065c5f04e811a8
Amadeus8182 <amadeus8182@gmail.com>
2023-02-01 21:17:05 -0600

    change to use embed for better aesthetic. now supports numbers. punctuations will not be added to the message

--- commands/bold.js
+++ commands/bold.js
@@ -4,14 +4,25 @@ module.exports = {
     name: 'bold',
 	desc: 'sends the user\'s given message using discord\'s regional indicator emojis.',
     execute(msg, args) {
+		let numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 		let send = "";
+
 		for(const s of args) {
 			for(const c of s) {
-				send += `:regional_indicator_${c.toLowerCase()}:`;
+				if(/[a-zA-Z]/.test(c)) send += `:regional_indicator_${c.toLowerCase()}:`;
+				else if(/[0-9]/.test(c)) send += `:${numbers[parseInt(c)]}:`;
 			}
+			send += ' ';
 		}
 
+
+		let embedding = new EmbedBuilder()
+			.setColor(parseInt((Math.random()*0xFFFFFF<<0).toString(16), 16))
+			.setDescription(send)
+			.setFooter({text: msg.author.username, iconURL: msg.author.displayAvatarURL({dynamic: true})}); 
+
+
 		if(msg.guild!==null) msg.delete();
-        msg.channel.send(send)
+        msg.channel.send({embeds: [embedding]});
     }
 }
