384485263df7e35e130c736f69879fd8c5dfbb03
Amadeus8182 <amadeus8182@gmail.com>
2023-02-01 21:54:25 -0600

    limit the allowed message between 1 to 180 characters. remove any non-alphanumeric character

--- commands/bold.js
+++ commands/bold.js
@@ -2,19 +2,26 @@ const { EmbedBuilder } = require("@discordjs/builders")
 
 module.exports = {
     name: 'bold',
-	desc: 'sends the user\'s given message using discord\'s regional indicator emojis.',
+	desc: 'sends the user\'s given message using discord\'s regional indicator emojis (removes any non-alphanumeric character).',
     execute(msg, args) {
 		let numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-		let send = "";
+		const regex = /[^a-zA-Z0-9]/g;
+		let sent = args.join(' ').replaceAll(regex, '');
+		let send = '';
 
-		for(const s of args) {
-			for(const c of s) {
-				if(/[a-zA-Z]/.test(c)) send += `:regional_indicator_${c.toLowerCase()}:`;
-				else if(/[0-9]/.test(c)) send += `:${numbers[parseInt(c)]}:`;
-			}
-			send += ' ';
+		if(sent.length > 180) {
+			msg.channel.send('Please use a message shorter than 180 characters!');
+			return;
+		} else if(sent.length < 1) {
+			msg.channel.send('Please provide a message to bolden!');
+			return;
 		}
 
+		for(const c of sent) {
+			if(/[a-zA-Z]/.test(c)) send += `:regional_indicator_${c.toLowerCase()}:`;
+			else if(/[0-9]/.test(c)) send += `:${numbers[parseInt(c)]}:`;
+			else if(c == ' ') send += c;
+		}
 
 		let embedding = new EmbedBuilder()
 			.setColor(parseInt((Math.random()*0xFFFFFF<<0).toString(16), 16))
